3f08aa3bde79615ab74764bd569e178362829de8
Michelle Villagran <mrvillagran16@gmail.com>
2023-04-20 20:22:56 -0600

    Add print over 60 function

--- friends.js
+++ friends.js
@@ -6,3 +6,12 @@ for (var i = 0; i < numbers.length; i++){
     }
 }
 
+function printOver60(numbers){
+    for (var i = 0; i < numbers.length; i++){
+        if (numbers[i] >= 60){
+            console.log(numbers[i])
+        }
+    }
+}
+
+printOver60(numbers)
